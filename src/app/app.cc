dbbb6d77c60b34ea7c45df66ff6fe63890a5d3c1
Christian Persch <chpe@src.gnome.org>
2022-03-01 23:38:31 +0100

    widget: Add terminal properties
    Part 1: API only, no-op implementation
    
    https://gitlab.gnome.org/GNOME/vte/-/issues/2125
    
    NOTE! Any modified or added code in this patch, and every following on this branch,
    is licensed under the GNU Affero General Public License, version 3, only, and
     *not* under the LGPL 3+!

--- src/app/app.cc
+++ src/app/app.cc
@@ -1376,6 +1376,36 @@ vteapp_terminal_class_init(VteappTerminalClass *klass)
 #endif
 
         gtk_widget_class_set_css_name(widget_class, "vteapp-terminal");
+
+        auto const vte_class = VTE_TERMINAL_CLASS(klass);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.valueless",
+                                            VTE_PROPERTY_VALUELESS,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.bool",
+                                            VTE_PROPERTY_BOOL,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.int",
+                                            VTE_PROPERTY_INT16,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.uint",
+                                            VTE_PROPERTY_UINT16,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.color",
+                                            VTE_PROPERTY_COLOR,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.string",
+                                            VTE_PROPERTY_STRING,
+                                            VTE_PROPERTY_FLAG_NONE);
+        vte_terminal_class_install_termprop(vte_class,
+                                            "vte.ext.vteapp.data",
+                                            VTE_PROPERTY_DATA,
+                                            VTE_PROPERTY_FLAG_NONE);
 }
 
 static void
@@ -2379,6 +2409,22 @@ window_selection_changed_cb(VteTerminal* terminal,
 }
 
 static void
+window_termprop_changed_cb(VteTerminal* terminal,
+                           char const* prop,
+                           VteappWindow* window)
+{
+        if (auto const value = vte::take_freeable
+            (vte_terminal_get_termprop_variant(terminal, prop))) {
+                auto str = vte::glib::take_string(g_variant_print(value.get(), true));
+                verbose_print("Termprop '%s' changed to '%s'\n",
+                              prop, str.get());
+        } else {
+                verbose_print("Termprop '%s' changed to no-value\n",
+                              prop);
+        }
+}
+
+static void
 window_input_enabled_state_cb(GAction* action,
                               GParamSpec* pspec,
                               VteappWindow* window)
@@ -2541,6 +2587,7 @@ vteapp_window_constructed(GObject *object)
         g_signal_connect(window->terminal, "resize-window", G_CALLBACK(window_resize_window_cb), window);
         g_signal_connect(window->terminal, "restore-window", G_CALLBACK(window_restore_window_cb), window);
         g_signal_connect(window->terminal, "selection-changed", G_CALLBACK(window_selection_changed_cb), window);
+        g_signal_connect(window->terminal, "termprop-changed", G_CALLBACK(window_termprop_changed_cb), window);
         g_signal_connect(window->terminal, "window-title-changed", G_CALLBACK(window_window_title_changed_cb), window);
         if (options.object_notifications)
                 g_signal_connect(window->terminal, "notify", G_CALLBACK(window_notify_cb), window);
